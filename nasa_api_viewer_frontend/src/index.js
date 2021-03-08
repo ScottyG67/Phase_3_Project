b4faa38175b5a3ca30e7855467dbb660e64ea371
ccchippp <clempke90@gmail.com>
2021-03-08 17:44:53 -0500

    added space below header

--- nasa_api_viewer_frontend/src/index.js
+++ nasa_api_viewer_frontend/src/index.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     // init()
     apodFetch()
     // marsFetch()
-    // fetchNasaImages()
+    fetchNasaImages()
     fetchWeekWeather()
     
     
