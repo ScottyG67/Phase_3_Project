99277b9edec0edff4a28979289ea7e991c34cb05
github username <scott.gloyna01@gmail.com>
2021-03-09 11:45:31 -0600

    slide show interval fixed

--- nasa_api_viewer_frontend/src/index.js
+++ nasa_api_viewer_frontend/src/index.js
@@ -4,82 +4,109 @@ const USERS_URL = `${BASE_URL}users/`
 const IMAGES_URL = `${BASE_URL}images/`
 const WEATHER_URL = `${BASE_URL}weathers/`
 const NASA_API_KEY = "nOK6nJhZT8gEU6dAhgYrHVQfki9F76TqYM1PTuNN"
-let slideIndex = 0
+
 
 document.addEventListener("DOMContentLoaded", ()=>{
     document.getElementById('nasa-image-search').addEventListener('submit', searchNasaApi)
-    init()
+    // build groups list and enable login
+    fetch(USERS_URL).then(res => res.json()).then(users => users.forEach(renderUsersList))
     
 })
 
-function init() {
-    fetch(USERS_URL).then(res => res.json()).then(users => users.forEach(renderUsersList))
-}
+// User / Interest group functions
+function renderUsersList(user){
+    const userList = document.querySelector('.dropdown_content')
+    const userItem = document.createElement('li')
+         userItem.innerText = user.username
+         userItem.dataset.userId = user.id
+         userItem.dataset.apod = user.apod
+         userItem.dataset.weather = user.weather
+         userItem.dataset.nasaImage = user.nasaimage
+         userItem.addEventListener('click',userLogin)
+     userList.append(userItem)
+ }
+ 
+ function userLogin(event) {
+     const userId = this.dataset.userId
+     sessionStorage.setItem("id",userId)
+     clearScreen()
+     if(this.dataset.apod === 'true'){
+         apodFetch()
+     }
+     if(this.dataset.nasaImage === 'true'){
+         document.querySelector('.nasa-images-section').style.display= 'block'
+         fetchNasaImages(this.innerText)
+     }
+     if(this.dataset.weather === 'true'){
+         fetchWeekWeather()
+     }
+     
+     fetchUserImages()
+ }
+ 
+ function clearScreen(){
+     document.querySelector('#nasa-images').innerHTML =''
+     document.querySelector('.nasa-images-section').style.display= 'none'
+     document.querySelector('.apod').innerHTML=''
+     document.querySelector('.mars_grid_container').innerHTML=''
+     document.querySelector('.user_images_container').innerHTML=''
+     document.querySelector('.user-images-section').style.display = "none"
+ }
+
+
+// Mars Weather
 
 function fetchWeekWeather() {
-    fetch(WEATHER_URL).then(res=>res.json()).then(weekWeather => weekWeather.forEach(renderWeather))
+    fetch(WEATHER_URL).then(res=>res.json()).then(weekWeather => {
+        const weatherGrid = document.querySelector('.mars_grid_container')
+        const title = document.createElement('h2')
+        title.innerText = "Weather Conditions on Mars"
+        title.className = 'mars_weather_title'
+        weatherGrid.appendChild(title)
+        weekWeather.forEach(renderWeather)})
 }
+
+function renderWeather(dayWeather) {
+    document.querySelector('.mars').style.display ='block'
+    const weatherGrid = document.querySelector('.mars_grid_container')
+    const weatherCard = document.createElement('div')
+        weatherCard.innerText = dayWeather.terrestrial_date
+        weatherCard.className = "mars_weather_card"
+    const weatherDay = document.createElement('ul')
+        weatherDay.dataset.dayId = dayWeather.id
+        weatherDay.className="mars_ul"
+    const maxTemp = document.createElement('li')
+        maxTemp.innerText = "High: " + dayWeather.max_temp + " C"
+    const minTemp = document.createElement('li')
+        minTemp.innerText = "Low: " + dayWeather.min_temp + " C"
+    const conditions = document.createElement('li')
+        conditions.innerText = "Visibility: " + dayWeather.atmo_opacity
+
+        weatherDay.append(maxTemp,minTemp,conditions)
+        weatherCard.appendChild(weatherDay)
+        weatherGrid.appendChild(weatherCard)
+}
+
+// APOD
+
 function apodFetch(){
     fetch("https://api.nasa.gov/planetary/apod?api_key="+NASA_API_KEY).then(res =>res.json()).then(img => renderApod(img))
 }
-function marsFetch(){
-    fetch("https://api.nasa.gov/insight_weather/?api_key="+NASA_API_KEY+"&feedtype=json&ver=1.0").then(res =>res.json()).then(marsData => renderMars(marsData))
-    // https://api.nasa.gov/insight_weather/?api_key=nOK6nJhZT8gEU6dAhgYrHVQfki9F76TqYM1PTuNN&feedtype=json&ver=1.0
+
+function renderApod(img){
+    const apodContainer = document.querySelector('.apod')
+    const apodImage = document.createElement('img')
+    apodImage.className = ('apod-img')
+    apodImage.src = img.url
+    apodContainer.append(apodImage)
 }
+
+
+// NASA Image Library
+
 function fetchNasaImages(searchTerm="nebula"){
-    fetch(NASA_URL+searchTerm).then(res => res.json()).then(stuff => stuff.collection.items.forEach(renderNasaImages))
-}
-
-function searchNasaApi(event) {
-    event.preventDefault()
-    const nasaImageContainer = document.querySelector('#nasa-images')
-    nasaImageContainer.innerHTML = ''
-    let search = event.target.text.value
-
-    fetch(NASA_URL+search)
-    .then(r => r.json())
-    .then(stuff => stuff.collection.items.forEach(renderNasaImages))
-}
-
-function renderUsersList(user){
-   const userList = document.querySelector('.dropdown_content')
-   const userItem = document.createElement('li')
-        userItem.innerText = user.username
-        userItem.dataset.userId = user.id
-        userItem.dataset.apod = user.apod
-        userItem.dataset.weather = user.weather
-        userItem.dataset.nasaImage = user.nasaimage
-        userItem.addEventListener('click',userLogin)
-    userList.append(userItem)
-}
-
-function userLogin(event) {
-    const userId = this.dataset.userId
-    sessionStorage.setItem("id",userId)
-    clearScreen()
     debugger
-    if(this.dataset.apod === 'true'){
-        apodFetch()
-    }
-
-    if(this.dataset.nasaImage === 'true'){
-        fetchNasaImages()
-    }
-
-    if(this.dataset.weather === 'true'){
-        fetchWeekWeather()
-    }
-
-    fetchUserImages()
-    
-}
-
-function clearScreen(){
-    document.querySelector('#nasa-images').innerHTML =''
-    document.querySelector('.apod').innerHTML=''
-    document.querySelector('.mars_grid_container').innerHTML=''
-    document.querySelector('.user_images_container').innerHTML=''
-    document.querySelector('.user-images-section').style.display = "none"
+    fetch(NASA_URL+searchTerm).then(res => res.json()).then(stuff => stuff.collection.items.forEach(renderNasaImages))
 }
 
 function renderNasaImages(nasaImage) {
@@ -101,14 +128,6 @@ function renderNasaImages(nasaImage) {
 
     nasaImageContainer.append(card)
     card.append(nasaImg, nasaTitle, saveButton)
-}
-
-function renderApod(img){
-    const apodContainer = document.querySelector('.apod')
-    const apodImage = document.createElement('img')
-    apodImage.className = ('apod-img')
-    apodImage.src = img.url
-    apodContainer.append(apodImage)
 }
 
 
@@ -138,26 +157,20 @@ function saveImage(img){
     fetch(IMAGES_URL, reqObj).then(res => res.json()).then(event.target.innerText="Image Saved")
 }
 
-function renderWeather(dayWeather) {
-    document.querySelector('.mars').style.display ='block'
-    const weatherGrid = document.querySelector('.mars_grid_container')
-    const weatherCard = document.createElement('div')
-        weatherCard.innerText = dayWeather.terrestrial_date
-        weatherCard.className = "mars_weather_card"
-    const weatherDay = document.createElement('ul')
-        weatherDay.dataset.dayId = dayWeather.id
-        weatherDay.className="mars_ul"
-    const maxTemp = document.createElement('li')
-        maxTemp.innerText = "High: " + dayWeather.max_temp + " C"
-    const minTemp = document.createElement('li')
-        minTemp.innerText = "Low: " + dayWeather.min_temp + " C"
-    const conditions = document.createElement('li')
-        conditions.innerText = "Visibility: " + dayWeather.atmo_opacity
+function searchNasaApi(event) {
+    event.preventDefault()
+    const nasaImageContainer = document.querySelector('#nasa-images')
+    nasaImageContainer.innerHTML = ''
+    let search = event.target.text.value
 
-        weatherDay.append(maxTemp,minTemp,conditions)
-        weatherCard.appendChild(weatherDay)
-        weatherGrid.appendChild(weatherCard)
+    fetch(NASA_URL+search)
+    .then(r => r.json())
+    .then(stuff => stuff.collection.items.forEach(renderNasaImages))
 }
+
+
+// Display User Saved Images
+
 
 function fetchUserImages() {
     const userId = sessionStorage.getItem("id")
@@ -171,8 +184,7 @@ function fetchUserImages() {
 }
 
 function renderUserImages(image){
-    console.log("creating card")
-    
+   
     const userImageContainer = document.querySelector(".user_images_container")
     const card = document.createElement('div')
     card.classList = 'user-image-card', 'fade'
@@ -191,46 +203,26 @@ function renderUserImages(image){
 }
 
 // Slide Show Functionality //
+let slideIndex = 1
+let slideChangeTimeout
 
 // Next/previous controls
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    clearTimeout(slideChangeTimeout)
+    slideIndex += n
+    console.log(slideIndex)
+    showSlides();
 }
-  
-// Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-// set slides to show
-//   function showSlides(n) {
-//       debugger
-//     let i;
-//     let slides = document.querySelectorAll(".user-image-card");
-//     // let dots = document.querySelector("dot");
-//     if (n > slides.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     // for (i = 0; i < dots.length; i++) {
-//     //     dots[i].className = dots[i].className.replace(" active", "");
-//     // }
-//     slides[slideIndex-1].style.display = "block";
-//     // dots[slideIndex-1].className += " active";
-// }
 
 // automatically rotate through
 function showSlides() {  
-    let slides = document.querySelectorAll(".user-image-card");
-    for (let i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
 
-    slideIndex++;
+    let slides = document.querySelectorAll(".user-image-card")
+
+    slides.forEach(slide => slide.style.display ="none")
     if (slideIndex > slides.length) {slideIndex = 1}
-
+    if (slideIndex < 1 ){slideIndex = slides.length}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 5000); // Change image every 5 seconds
+    slideIndex++
+    slideChangeTimeout = setTimeout(showSlides,5000)
   }
-
